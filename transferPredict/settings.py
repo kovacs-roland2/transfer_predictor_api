5cc6568077526697452ff9b5a113ca2de480e038
Roland Kovács <Roland.Kovacs4@ibm.com>
2022-05-30 14:34:51 +0200

    environ set up

--- transferPredict/settings.py
+++ transferPredict/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+# import environ
 import os
 import django_heroku
 
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+# env = environ.Env()
+# # reading .env file
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', 1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
