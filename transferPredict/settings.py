5897256161291eaa9d08279e50e25bebc970cdc9
Roland Kovács <Roland.Kovacs4@ibm.com>
2022-05-30 14:49:48 +0200

    add allowed hosts

--- transferPredict/settings.py
+++ transferPredict/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['transfer-predictor-backend.herokuapp.com',]
 
 
 # Application definition
