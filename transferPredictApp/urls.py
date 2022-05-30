ab6c7e37b5ece072e5e816c2e129b08a8f91d251
Roland Kovács <Roland.Kovacs4@ibm.com>
2022-05-30 14:57:08 +0200

    change url to path

--- transferPredictApp/urls.py
+++ transferPredictApp/urls.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url('overview/', views.home, name = 'home'),
-    url('tournament/', views.players_tournament_filter, name = 'players_tournament'),
-    url('players/', views.search_players, name = 'search_players'),
-    url('playerinfo/', views.get_player, name = 'get_player'),
-    url('radarplot/', views.get_radar_plot, name = 'radarplot'),
-    url('violinplot/', views.get_violin_plot, name = 'violinplot'),
-    url('scatterplot/', views.get_scatter_plot, name = 'scatterplot'),
-    # url('similarity/', views.get_similarity, name = 'similarity'),
-    url('similarity_plot', views.get_similarity_plot, name = 'similarity_plot'),
-    url('predictions/', views.get_predictions, name = 'predictions')
+    path('overview/', views.home, name = 'home'),
+    path('tournament/', views.players_tournament_filter, name = 'players_tournament'),
+    path('players/', views.search_players, name = 'search_players'),
+    path('playerinfo/', views.get_player, name = 'get_player'),
+    path('radarplot/', views.get_radar_plot, name = 'radarplot'),
+    path('violinplot/', views.get_violin_plot, name = 'violinplot'),
+    path('scatterplot/', views.get_scatter_plot, name = 'scatterplot'),
+    # path('similarity/', views.get_similarity, name = 'similarity'),
+    path('similarity_plot', views.get_similarity_plot, name = 'similarity_plot'),
+    path('predictions/', views.get_predictions, name = 'predictions')
 ]
