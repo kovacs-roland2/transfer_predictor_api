9dc6fc8bd2145a9659bb18c8196617f5698eff4a
Roland Kovács <Roland.Kovacs4@ibm.com>
2022-05-30 11:14:01 +0200

    files movement to root

--- transferPredict/settings.py
+++ transferPredict/settings.py
@@ -81,7 +81,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
-    "transfer-predictor.herokuapp.com",
+    "https://transfer-predictor.herokuapp.com",
 ]
 
 
